7fad998397bbe2dfdb7226bb49b94e8eb803b606
Jordan Colubiale <jordancolubiale@Jordans-Air-2.attlocal.net>
2022-11-11 14:34:40 -0500

    database created

--- Develop/db/schema.sql
+++ Develop/db/schema.sql
@@ -1,2 +1,21 @@
-DROP DATABASE IF EXISTS crowdfund_db;
-CREATE DATABASE crowdfund_db;
+DROP DATABASE IF EXISTS jobsearcher_db;
+CREATE DATABASE jobsearcher_db;
+
+USE jobsearcher_db;
+
+CREATE TABLE profile (
+id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+first_name VARCHAR(30),
+last_name VARCHAR(30),
+bio TEXT,
+skills SET ,
+education SET,
+social_media SET
+);
+
+CREATE TABLE jobs (
+id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+job_title VARCHAR(30),
+job_description TEXT,
+salary DECIMAL
+);
