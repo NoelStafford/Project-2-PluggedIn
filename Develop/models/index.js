7ab3a600f9403ae817fd9fff8ba094efce0424bf
NoelStafford <noeljordan34@gmail.com>
2022-11-15 09:59:09 -0600

    models should be done

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -1,14 +1,5 @@
 const Job = require('./Job');
 const Profile = require('./Profile');
 
-Profile
-.hasMany(Job, {
-  foreignKey: 'profile_id',
-  onDelete: 'CASCADE'
-});
-
-Job.belongsTo(Profile, {
-  foreignKey: 'profile_id'
-});
 
 module.exports = { Profile, Job };
