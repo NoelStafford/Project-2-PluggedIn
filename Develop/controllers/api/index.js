3a7d195bf5eefb2fd15096e73ec28e5357d10eb9
Andrew PG <andrewpgibbs71@gmail.com>
2022-11-15 23:34:59 -0500

    api file is now connected to the api index

--- Develop/controllers/api/index.js
+++ Develop/controllers/api/index.js
@@ -1,8 +1,10 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
 const projectRoutes = require('./projectRoutes');
+const apiFetch = require('./apiFetch');
 
 router.use('/users', userRoutes);
 router.use('/projects', projectRoutes);
+router.use('/apifetch', apiFetch)
 
 module.exports = router;
