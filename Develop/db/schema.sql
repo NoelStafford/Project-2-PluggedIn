d3f75c6e7b955ace1b1cb3e1b73813e9c77dff0c
Andrew PG <andrewpgibbs71@gmail.com>
2022-11-16 01:24:08 -0500

    commented out schema so that we only have the first 2 lines

--- Develop/db/schema.sql
+++ Develop/db/schema.sql
@@ -1,23 +1,23 @@
 DROP DATABASE IF EXISTS jobsearcher_db;
 CREATE DATABASE jobsearcher_db;
 
-USE jobsearcher_db;
+-- USE jobsearcher_db;
 
-CREATE TABLE profile (
-id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-first_name VARCHAR(30),
-last_name VARCHAR(30),
-password VARCHAR (30), 
-email VARCHAR (30)
-bio TEXT,
-skills VARCHAR(255) ,
-education VARCHAR(255),
-social_media VARCHAR(255)
-);
+-- CREATE TABLE profile (
+-- id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+-- first_name VARCHAR(30),
+-- last_name VARCHAR(30),
+-- password VARCHAR (30), 
+-- email VARCHAR (30)
+-- bio TEXT,
+-- skills VARCHAR(255) ,
+-- education VARCHAR(255),
+-- social_media VARCHAR(255)
+-- );
 
-CREATE TABLE jobs (
-id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-job_title VARCHAR(30),
-job_description TEXT,
-salary DECIMAL
-);
+-- CREATE TABLE jobs (
+-- id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+-- job_title VARCHAR(30),
+-- job_description TEXT,
+-- salary DECIMAL
+-- );
